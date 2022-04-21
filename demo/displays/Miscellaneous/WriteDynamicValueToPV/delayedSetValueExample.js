3f086b954b44bb1365d50f909fbc41213b4f8490
Fabian Diet <fabian.diet@spaceapplications.com>
2022-04-21 11:06:35 +0200

    Update example displays

--- demo/displays/Miscellaneous/WriteDynamicValueToPV/delayedSetValueExample.js
+++ demo/displays/Miscellaneous/WriteDynamicValueToPV/delayedSetValueExample.js
@@ -1,17 +1,14 @@
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-importPackage(Packages.java.lang);
+var Runnable = Java.type("java.lang.Runnable");
+var Thread = Java.type("java.lang.Thread");
 
-
-runnable = {
-	run:function()
-		{	
-			for(var i=5; i>0; i--){
-				if(!display.isActive())
-					return;			
-				Thread.sleep(1000);
+new Thread(new Runnable({
+	run: function() {
+		for (var i = 5; i > 0; i--) {
+			if (!display.isActive()) {
+				return;
 			}
-			pvs[1].setValue(PVUtil.getLong(pvs[0]))			
-		}	
-	};		
-
-new Thread(new Runnable(runnable)).start();
+			Thread.sleep(1000);
+		}
+		pvs[1].setValue(PVUtil.getLong(pvs[0]));
+	}
+})).start();
--- demo/displays/scripts/KnobValueDialog.js
+++ demo/displays/scripts/KnobValueDialog.js
@@ -1,23 +1,16 @@
-importPackage(Packages.org.eclipse.jface.dialogs);
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-
 var flagName = "popped";
 
-if(widget.getExternalObject(flagName) == null){
-	widget.setExternalObject(flagName, false);	
+if (widget.getVar(flagName) == null) {
+	widget.setVar(flagName, false);	
 }
 
-var b = widget.getExternalObject(flagName);
+var b = widget.getVar(flagName);
 
-if(PVUtil.getDouble(pvs[0]) > 80){		
-		if( b == false){
-			widget.setExternalObject(flagName, true);
-			MessageDialog.openWarning(
-				null, "Warning", "The temperature you set is too high!");
+if (PVUtil.getDouble(pvs[0]) > 80) {
+		if (b == false) {
+			widget.setVar(flagName, true);
+			GUIUtil.openWarningDialog("The temperature you set is too high!");
 		}
-		
-}else if (b == true){
-	widget.setExternalObject(flagName, false);
+} else if (b == true) {
+	widget.setVar(flagName, false);
 }
-	
-
