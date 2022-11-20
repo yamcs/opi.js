2d116813f9280b485b27c12a178bf2a8096f1c9c
Fabian Diet <fabian.diet@spaceapplications.com>
2022-11-20 09:12:53 +0100

    Upgrade deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import copy from 'rollup-plugin-copy';
-import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
